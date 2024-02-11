2ee7c35e73b86ab8e8820aefba7356311f3af290
Connor Krohn <12523531+connorkrohn@users.noreply.github.com>
2024-02-11 06:29:51 +0000

    + layout styling

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,4 +2,10 @@
 	import '../app.pcss';
 </script>
 
-<slot />
+<header class="fixed flex items-center top-0 inset-x-0 h-12 m-4 px-2 bg-background border rounded-xl shadow-lg">
+	<span>Wishlist</span>
+</header>
+
+<div class="grid place-items-center text-center">
+	<slot />
+</div>
