f414b3bd4b00a6fba21d029a6a6a0946b6136184
Connor Krohn <12523531+connorkrohn@users.noreply.github.com>
2024-02-12 01:55:55 +0000

    + cards

---
+++ src/lib/components/ui/card/card.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import type { HTMLAttributes } from "svelte/elements";
+	import { cn } from "$lib/utils";
+
+	type $$Props = HTMLAttributes<HTMLDivElement>;
+
+	let className: $$Props["class"] = undefined;
+	export { className as class };
+</script>
+
+<!-- svelte-ignore a11y-no-static-element-interactions -->
+<div
+	class={cn("rounded-xl border bg-card text-card-foreground shadow grid grid-cols-[minmax(0px,_40%)_1fr] p-3 gap-x-3 text-left", className)}
+	{...$$restProps}
+	on:click
+	on:focusin
+	on:focusout
+	on:mouseenter
+	on:mouseleave
+>
+	<slot />
+</div>
