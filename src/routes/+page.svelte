3fd2cec4ead209d93da673adc705c72151a58dfc
Connor Krohn <ckrohn1@gmail.com>
2024-02-16 12:01:29 +1000

    feat: add badges in card footer

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script>
     import * as Card from "$lib/components/ui/card";
+    import { Badge } from "$lib/components/ui/badge";
     let each = [0,0,0,0,0,0,0,0,0,0]
 </script>
 
@@ -17,7 +18,9 @@
           <p>Card Content</p>
         </Card.Content>
         <Card.Footer>
-          <p>Card Footer</p>
+          <Badge variant="secondary">Amazon</Badge>
+          <Badge variant="secondary">The Iconic</Badge>
+          <Badge variant="secondary">Rebel</Badge>
         </Card.Footer>
       </Card.Root>
       {/each}
