b461481cc1965b3bee810d37dd09535aa8d46672
Connor Krohn <ckrohn1@gmail.com>
2024-02-18 17:36:47 +1000

    feat: update wishlist data loading from Notion database

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,18 +5,17 @@
     let each = [0,0,0,0,0,0,0,0,0,0]
 
     export let data: PageData;
-    console.log(data);
 </script>
 
 <div class="w-full max-w-2xl mx-auto flex flex-col gap-4">
-    {#each each as item, i}
+    {#each data.wishlist as item, i}
     <Card.Root>
         <Card.Image>
             <img src="https://source.unsplash.com/random/500x500/?product&sig={i + 1}" alt="">
         </Card.Image>
         <Card.Header>
-          <Card.Title>Card Title</Card.Title>
-          <Card.Description>Card Description</Card.Description>
+          <Card.Title>{item.title}</Card.Title>
+          <Card.Description>${item.price ? item.price : "--"}</Card.Description>
         </Card.Header>
         <Card.Content>
           <p>Card Content</p>
