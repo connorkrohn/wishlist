7639a17046399917abe156232f3b7457e10904d9
Connor Krohn <ckrohn1@gmail.com>
2024-03-22 16:20:22 +1000

    refactor: stream loading and rendering of wishlist items.

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,31 +1,41 @@
 import type { PageServerLoad } from './$types';
 import { NOTION_WISHLIST_SECRET, NOTION_WISHLIST_ID } from '$env/static/private';
-import { Client } from '@notionhq/client';
+import { Client, isFullPage } from '@notionhq/client';
+
+// custom types based off return Notion JSON
+export interface WishlistItem {
+	Title: { title: [{ plain_text: string }] };
+	Price: { number: number };
+	Image: { files: [{ external: { url: string }; file: { url: string } }] };
+	Notes: { rich_text: [{ plain_text: string }] };
+}
 
 // Initializing a client
-const notion = new Client({
-	auth: NOTION_WISHLIST_SECRET
-});
+const notion = new Client({ auth: NOTION_WISHLIST_SECRET });
 
 export const load: PageServerLoad = async () => {
-  let wishlist: { title: string, price: number, image: string, note: string }[] = [];
-	const { results } = await notion.databases.query({ database_id: NOTION_WISHLIST_ID });
-
-	// eslint-disable-next-line @typescript-eslint/no-explicit-any
-	results.map((e: any) => {
-        const resultsProps = e.properties;
-        wishlist = [
-            ...wishlist,
-            {
-                title: resultsProps.Title.title[0].plain_text,
-                price: resultsProps.Price.number,
-                image: resultsProps.Image.files[0]?.external?.url ? resultsProps.Image.files[0]?.external?.url : resultsProps.Image.files[0]?.file?.url,
-                note: resultsProps.Notes.rich_text[0]?.plain_text
-            }
-        ];
-    });
-
+	// PagaData
 	return {
-		wishlist: wishlist,
+		wishlist: streamNotionData()
 	};
+	// get Notion data via Promise
+	async function streamNotionData() {
+		let wishlist: { id: string; title: string; price: number; image: string; note: string }[] = [];
+		const { results } = await notion.databases.query({ database_id: NOTION_WISHLIST_ID });
+		for (const page of results) {
+			if (!isFullPage(page)) continue;
+			const wishlistItem = page.properties as unknown as WishlistItem;
+			wishlist = [
+				...wishlist,
+				{
+					id: page.id,
+					title: wishlistItem.Title.title[0].plain_text,
+					price: wishlistItem.Price.number,
+					image: wishlistItem.Image.files[0]?.external?.url ? wishlistItem.Image.files[0]?.external?.url : wishlistItem.Image.files[0]?.file?.url,
+					note: wishlistItem.Notes.rich_text[0]?.plain_text
+				}
+			];
+		}
+		return wishlist;
+	}
 };
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,40 +1,40 @@
 <script lang="ts">
-    import type { PageData } from './$types';
-    import * as Card from "$lib/components/ui/card";
-    import { Badge } from "$lib/components/ui/badge";
-    let each = [0,0,0,0,0,0,0,0,0,0]
+	import type { PageData } from './$types';
+	import * as Card from '$lib/components/ui/card';
 
-    export let data: PageData;
+	export let data: PageData;
 </script>
 
-<div class="w-full max-w-xl mx-auto flex flex-col gap-4">
-    {#each data.wishlist as item, i}
-    <Card.Root>
-        <Card.Image>
-            {#if item.image}
-                <img src={item.image} alt="">
-            {:else}
-                <div class="grid place-items-center h-full bg-muted">
-                    <span>No Image</span>
-                </div>
-            {/if}
-        </Card.Image>
-        <Card.Header>
-          <Card.Title>{item.title}</Card.Title>
-          <Card.Description>${item.price ? item.price : "--"}</Card.Description>
-        </Card.Header>
-        {#if item.note}
-            <Card.Content>
-              <p>{item.note}</p>
-            </Card.Content>
-        {/if}
-        <!-- <Card.Footer>
+<div class="mx-auto flex w-full max-w-xl flex-col gap-4">
+	{#await data.wishlist then wishlist}
+		{#each wishlist as item, i}
+			<Card.Root>
+				<Card.Image>
+					{#if item.image}
+						<img src={item.image} alt="" />
+					{:else}
+						<div class="grid h-full place-items-center bg-muted">
+							<span>No Image</span>
+						</div>
+					{/if}
+				</Card.Image>
+				<Card.Header>
+					<Card.Title>{item.title}</Card.Title>
+					<Card.Description>${item.price ? item.price : '--'}</Card.Description>
+				</Card.Header>
+				{#if item.note}
+					<Card.Content>
+						<p>{item.note}</p>
+					</Card.Content>
+				{/if}
+				<!-- <Card.Footer>
           <div class="flex gap-2">
             <Badge variant="default">Amazon</Badge>
             <Badge variant="default">The Iconic</Badge>
             <Badge variant="default">Rebel</Badge>
           </div>
         </Card.Footer> -->
-      </Card.Root>
-      {/each}
+			</Card.Root>
+		{/each}
+	{/await}
 </div>
