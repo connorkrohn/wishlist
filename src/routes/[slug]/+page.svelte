2a0429d64b0c6bf95d914c26def21cf056c2bdbb
Connor Krohn <ckrohn1@gmail.com>
2024-04-19 11:00:29 +1000

    feat: added item pages

---
+++ src/routes/[slug]/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import type { PageData } from './$types';
+  import { page } from '$app/stores';
+
+	export let data: PageData;
+</script>
+
+<div class="mx-auto flex w-full max-w-xl flex-col gap-4">
+  {#await data.wishlist then wishlist}
+    <img src="{wishlist.filter((obj)=>obj.id == $page.params.slug)[0].image}" alt="">
+    <p>{wishlist.filter((obj)=>obj.id == $page.params.slug)[0].title}</p>
+  {/await}
+</div>
