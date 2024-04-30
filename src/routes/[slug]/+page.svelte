44b376eda57706de66de91dbe897f0186c415cc2
Connor Krohn <ckrohn1@gmail.com>
2024-04-30 11:24:32 +1000

    format

--- src/routes/[slug]/+page.svelte
+++ src/routes/[slug]/+page.svelte
@@ -7,59 +7,60 @@
 
 	export let data: PageData;
 
-  const item = derived(count, $count => $count?.filter((obj) => obj.id == $page.params.slug)[0]);
-  const itemIndex = derived(count, $count => $count?.findIndex((obj) => obj.id == $page.params.slug));
+	const item = derived(count, ($count) => $count?.filter((obj) => obj.id == $page.params.slug)[0]);
+	const itemIndex = derived(count, ($count) =>
+		$count?.findIndex((obj) => obj.id == $page.params.slug)
+	);
 
-  let itemStreamed: Awaited<typeof data.streamed.wishlist>[0],
-  itemStreamedIndex: number;
-  
-  data.streamed.wishlist.then(result => {
-    itemStreamed = result.filter((obj) => obj.id == $page.params.slug)[0]
-    itemStreamedIndex = result.findIndex((obj) => obj.id == $page.params.slug)
-  })
+	let itemStreamed: Awaited<typeof data.streamed.wishlist>[0], itemStreamedIndex: number;
+
+	data.streamed.wishlist.then((result) => {
+		itemStreamed = result.filter((obj) => obj.id == $page.params.slug)[0];
+		itemStreamedIndex = result.findIndex((obj) => obj.id == $page.params.slug);
+	});
 </script>
 
 <div class="mx-auto flex w-full max-w-xl flex-col gap-4">
 	{#if data.isDataRequest && $count !== undefined}
-  <div class="aspect-square">
-		{#if $item.image}
-			<img
-				alt=""
-				src={$item.image}
-				data-flip-id="image{$itemIndex}"
-				class="aspect-square rounded-xl bg-white object-contain"
-			/>
-		{:else}
-			<div
-				data-flip-id="image{$itemIndex}"
-				class="grid aspect-square place-items-center rounded-xl bg-muted"
-			>
-				<span>No Image</span>
-			</div>
-		{/if}
-    </div>
-    <div>{$item.title}</div>
+		<div class="aspect-square">
+			{#if $item.image}
+				<img
+					alt=""
+					src={$item.image}
+					data-flip-id="image{$itemIndex}"
+					class="aspect-square rounded-xl bg-white object-contain"
+				/>
+			{:else}
+				<div
+					data-flip-id="image{$itemIndex}"
+					class="grid aspect-square place-items-center rounded-xl bg-muted"
+				>
+					<span>No Image</span>
+				</div>
+			{/if}
+		</div>
+		<div>{$item.title}</div>
 	{:else}
 		{#await data.streamed.wishlist}
 			<Skeleton class="grid aspect-square" />
 		{:then}
-    <div class="aspect-square">
-      {#if itemStreamed.image}
-			<img
-				alt=""
-				src={itemStreamed.image}
-				data-flip-id="image{itemStreamedIndex}"
-				class="aspect-square rounded-xl bg-white object-contain"
-			/>
-		{:else}
-			<div
-				data-flip-id="image{itemStreamedIndex}"
-				class="grid aspect-square place-items-center rounded-xl bg-muted"
-			>
-				<span>No Image</span>
+			<div class="aspect-square">
+				{#if itemStreamed.image}
+					<img
+						alt=""
+						src={itemStreamed.image}
+						data-flip-id="image{itemStreamedIndex}"
+						class="aspect-square rounded-xl bg-white object-contain"
+					/>
+				{:else}
+					<div
+						data-flip-id="image{itemStreamedIndex}"
+						class="grid aspect-square place-items-center rounded-xl bg-muted"
+					>
+						<span>No Image</span>
+					</div>
+				{/if}
 			</div>
-		{/if}
-    </div>
 		{/await}
 	{/if}
 </div>
